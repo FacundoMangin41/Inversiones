b290109c118593a49d3042909284e04e2f8d040e
Facundo Mangin <63527441+FacundoMangin41@users.noreply.github.com>
2024-06-30 23:26:04 -0300

    Update AppRouter.jsx

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,42 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Routes } from "react-router-dom";
 import Layout from '../layout/Layout';
 import TablaPage from "../pages/tabla/AppTabla";
 import AppFormulario from "../pages/formulario/AppFormulario";
 import AppEstadisticas from "../pages/Estadisticas/AppEstadisticas";
-
+import Swal from 'sweetalert2';
 
 const AppRouter = () => {
+    useEffect(() => {
+        Swal.fire({
+            title: '<span style="color: #c24e00; font-weight: BOLD;">¡IMPORTANTE!</span>',
+            html: 'Para llevar un correcto control, debe <br><span style="font-size: 20px; font-weight: bold;">"INVERTIR SIEMPRE TODO EL DINERO DE SU CUENTA EN CADA OPERACIÓN"</span> <br>Caso contrario, las estadísticas no serán correctas.',
+            icon: "warning"
+        });
+    }, []);
+
     return (
         <div>
             <>
                 <Routes>
-
                     <Route path='/' element={<Layout />}>
-
                         <Route
                             index
                             element={<TablaPage />}
                         />
-
                         <Route
                             path="/tabla"
                             element={<TablaPage />}
                         />
-
-<Route
+                        <Route
                             path="/formulario"
                             element={<AppFormulario />}
                         />
-
-<Route
+                        <Route
                             path="/estadisticas"
                             element={<AppEstadisticas />}
                         />
-
-
                     </Route>
-
                 </Routes>
             </>
         </div>
